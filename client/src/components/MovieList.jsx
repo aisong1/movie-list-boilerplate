dc9f9ba412723b390e27da34cf51270e5f3b1415
Andrew Song <andrewsong61@gmail.com>
2020-11-03 22:56:10 -0800

    Add Watched and To Watch buttons

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import MovieListItem from './MovieListItem.jsx';
 
 var MovieList = (props) => (
-  // <>
-  // <div>
-  //   <button>Watched</button>
-  //   <button>To Watch</button>
-  // </div>
-  <table>
-    <tbody>
-      {props.movies.map((movie) => (
-        <MovieListItem key={movie.title} movie={movie} />
-      ))}
-    </tbody>
-  </table>
-  // </> 
+  <div className="movieList">
+    <div className='tabs'>
+      <button>Watched</button>
+      <button>To Watch</button>
+    </div>
+    <table>
+      <tbody>
+        {props.movies.map((movie) => (
+          <MovieListItem key={movie.title} movie={movie} />
+        ))}
+      </tbody>
+    </table>
+  </div> 
 )
 
 export default MovieList;
